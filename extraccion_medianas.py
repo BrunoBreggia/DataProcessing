308a7447bfc11763e7eb301de696ad1428d7accd
Bruno Breggia <breggiabruno@gmail.com>
2024-07-24 13:17:10 -0300

    Creo script para extracción de medianas para mandar al cluster

--- extraccion_medianas.py
+++ extraccion_medianas.py
@@ -1,12 +1,60 @@
 import os
-import seaborn as sns
+import json
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from itertools import product
 from df_assembly01 import dataframe_assembly
+from itertools import product
 
 RUTA_SIM = "../SimulacionMine/sim06_nwEv/"
 ARCHI_MEDIANAS = "sim06_data.csv"
+
+
+def dataframe_assembly(filename: str) -> pd.DataFrame:
+    """
+    Funcion que recibe nombre de archivo json y devuelve dataframe con
+    los datos de las simulaciones y los parametros estadisticos del conjunto de
+    sus realizaciones
+
+    :param filename: nombre del archivo json
+    :return: DataFrame
+    """
+
+    with open(filename, 'r') as jsonFile:
+        content = json.load(jsonFile)
+        # content is a dictionary
+
+    # create pandas dataframes
+    params_df = pd.DataFrame.from_dict([content['params']])
+    data_df = pd.DataFrame.from_dict(content['data'])
+
+    # Agrego datos del json de parametros
+    data_df['capas'] = params_df['capa'].get(0)
+    data_df['tipo'] = params_df['tipo'].get(0)
+    data_df['realizaciones'] = params_df['realizaciones'].get(0)
+    data_df['seed'] = params_df['seed'].get(0)
+    data_df['data_mu'] = [params_df['data_mu'].get(0)] * len(data_df)
+
+    # Agrego datos de tendencia central de las realizaciones
+    data_df['training_mean'] = [np.mean(values) for values in data_df['im_entrenamiento']]
+    data_df['testing_mean'] = [np.mean(values) for values in data_df['im_testeo']]
+    data_df['training_med'] = [np.median(values) for values in data_df['im_entrenamiento']]
+    data_df['testing_med'] = [np.median(values) for values in data_df['im_testeo']]
+
+    # Agrego datos de dispersion de las realizaciones
+    data_df['training_var'] = [np.var(values) for values in data_df['im_entrenamiento']]
+    data_df['testing_var'] = [np.var(values) for values in data_df['im_testeo']]
+    data_df['training_std'] = np.sqrt(data_df['training_var'])
+    data_df['testing_std'] = np.sqrt(data_df['testing_var'])
+    data_df['training_max'] = [np.max(values) for values in data_df['im_entrenamiento']]
+    data_df['testing_max'] = [np.max(values) for values in data_df['im_testeo']]
+    data_df['training_min'] = [np.min(values) for values in data_df['im_entrenamiento']]
+    data_df['testing_min'] = [np.min(values) for values in data_df['im_testeo']]
+
+    # Extraidos los datos estadisticos de las realizaciones, las elimino
+    del data_df['im_entrenamiento']
+    del data_df['im_testeo']
+
+    return data_df
 
 
 def dataframe_from_directory(dirname: str):
@@ -45,7 +93,7 @@ def dataframe_from_directory(dirname: str):
 df = dataframe_from_directory(RUTA_SIM)
 df.to_csv(ARCHI_MEDIANAS)
 
-"""
+
 # Read data from sim06 file
 # df = pd.read_csv(ARCHI_MEDIANAS)
 
@@ -89,6 +137,8 @@ for suj, comb, cic in product(sujetos, combinaciones, ciclos):
                                        ]
 df_datos.to_csv("sim06_medianas_realizaciones.csv")
 
+
+"""
 # Graficacion particular
 fig, axs = plt.subplots(2, 1, sharey=True, sharex=True)
 CICLO = ["full", "swing", "stance", "nods"][0]  # cambiar este indice para probar con cada ciclo
